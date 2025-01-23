bd9faea2f93762571d79c37e3d33eefd3483eeab
Muhammad Ali Nizami <kustalics@gmail.com>
2025-01-23 15:47:48 +0500

    First Commit

---
+++ android/app/src/main/kotlin/com/example/flutter_b11_firebase/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_b11_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/services/task.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_b11_firebase/models/task.dart';
+
+class TaskServices {
+  ///Create Task
+  Future createTask(TaskModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .add(model.toJson());
+  }
+
+  ///Update Task
+  Future updateTask(TaskModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(model.docId)
+        .update({
+      "title": model.title,
+      "description": model.description,
+      "image": model.image,
+    });
+  }
+
+  ///Delete Task
+  Future deleteTask(String taskID) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(taskID)
+        .delete();
+  }
+
+  ///Get All Tasks
+  ///Get Completed Tasks
+  ///Get InCompleted Tasks
+  ///Mark task as Complete
+  Future markTaskAsComplete(String taskID) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(taskID)
+        .update({'isCompleted': true});
+  }
+}
